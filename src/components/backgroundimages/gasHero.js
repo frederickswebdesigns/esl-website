3e702300fc08ac475e6fcbcd69ae1a041fe945f1
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2020-06-10 18:23:05 +1000

    changed images

--- src/components/backgroundimages/gasHero.js
+++ src/components/backgroundimages/gasHero.js
@@ -8,7 +8,7 @@ const BackgroundSection = ({ className }) => (
   <StaticQuery
     query={graphql`
       query {
-        desktop: file(relativePath: { eq: "gas-line-warning.JPG" }) {
+        desktop: file(relativePath: { eq: "gas-line-locating.JPG" }) {
           childImageSharp {
             fluid(quality: 90, maxWidth: 1260) {
               ...GatsbyImageSharpFluid_withWebp
